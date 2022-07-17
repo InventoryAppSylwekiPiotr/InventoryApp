ec956575302c2c369f885046431815e65b1c1aca
uzytkownik <uzytkownik@kursGIT.p1>
2022-07-17 11:32:25 +0200

    project init

--- src/test/java/pl/sda/inventory/inventoryapp/InventoryAppApplicationTests.java
+++ src/test/java/pl/sda/inventory/inventoryapp/InventoryAppApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class InventoryAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
